624c9ca8521a8777a9e36c35b305a5678f84dc67
baekwinter <154979542+baekwinter@users.noreply.github.com>
2024-01-22 09:15:05 +0900

    [level 0] Title: 각도기, Time: 0.15 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.cs
+++ 프로그래머스/0/120829. 각도기/각도기.cs
@@ -3,23 +3,6 @@ using System;
 public class Solution {
     public int solution(int angle) {
         int answer = 0;
-        
-        if(angle < 90)
-        {
-            angle = 1;
-        }
-         else if(angle == 90)
-        {
-            angle = 2;
-        }
-         else if(angle < 180)
-        {
-            angle = 3;
-        }
-         else if(angle == 180)
-        {
-            angle = 4;
-        }
-        return angle;
+        return answer;
     }
 }
